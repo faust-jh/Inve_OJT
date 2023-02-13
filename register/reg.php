40ba77b7be2d0cd9960a4d11e8de2a5958fb0fe9
faust-jh <faustinojheru@gmail.com>
2023-02-13 22:06:48 +0800

    Di pa tapos yung reg.php

--- register/reg.php
+++ register/reg.php
@@ -1,7 +1,6 @@
 <?php
 $server = "localhost";
 $username = "root";
-$num = 1;
 $con = mysqli_connect($server, $username);
 if (!$con) {
     echo ("Can't connect!");
@@ -10,11 +9,31 @@ if (!$con) {
 $compname = $_POST['companyname'];
 $compmail = $_POST['companyemail'];
 $comppass = $_POST['companypass'];
-
 mysqli_select_db($con, "inventory_system");
-$sql2 = "INSERT INTO company_account (CompanyEmail, CompanyPassword, CompanyID) VALUES ('$compmail', '$comppass', '$num')";
+
+/*$sql1 = "SELECT MAX(CompanyID) FROM company_profile";
+$query = mysqli_query($con, $sql1);
+$result = mysqli_fetch_assoc($query);*/
+
+$query = "SELECT CompanyID FROM company_profile ORDER BY CompanyID DESC limit 1";
+$result = mysqli_query($con, $query);
+$row = mysqli_fetch_array($result);
+$first = $row['0'];
+echo $first;
+
+/*if ($idContainer > 0) {
+$idContainer = $idContainer + 1;
+$sql2 = "INSERT INTO company_account (CompanyEmail, CompanyPassword, CompanyID) VALUES ('$compmail', '$comppass', '$idContainer')";
 $sql = "INSERT INTO company_profile (CompanyName) VALUES ('$compname')";
 
 mysqli_query($con, $sql2);
 mysqli_query($con, $sql);
 mysqli_close($con);
+}*//*else {
+$sql2 = "INSERT INTO company_account (CompanyEmail, CompanyPassword, CompanyID) VALUES ('$compmail', '$comppass', 1)";
+$sql = "INSERT INTO company_profile (CompanyName) VALUES ('$compname')";
+
+mysqli_query($con, $sql2);
+mysqli_query($con, $sql);
+mysqli_close($con);
+}*/

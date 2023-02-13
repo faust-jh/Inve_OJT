3fe20afb59741bedacaebe30db3d8e4bcabdcd08
darwin2919 <124544012+darwin2919@users.noreply.github.com>
2023-02-13 13:39:13 +0800

    Update aboutus.php

--- aboutus/aboutus.php
+++ aboutus/aboutus.php
@@ -1,96 +1,96 @@
 <?php
-include '../includes/header.php';
-include '../includes/navbar.php';
+include 'includes/header.php';
+include 'includes/navbar.php';
 ?>
 
 <div class="aboutBg">
-
+    
     <div class="about_container">
         <h4 class="aboutHead">Lorem ipsum dolor sit amet.</h4>
-
+        
         <div class="aboutRow">
 
             <div class="aboutRow_sub row">
-
+            
                 <div class="aboutCol col">
                     <div class="row">
-                        <div class="col-1">
-                            <div class="aboutIcon">
-                                <img src="../design/images/arrowup.png" class="p-1" alt="..." style="height: 50px; width:50px;background:none; ">
-                            </div>
+                    <div class="col-1">
+                        <div class="aboutIcon">
+                            <img src="design/photos/arrowup.png" class="p-1" alt="..." style="height: 50px; width:50px;background:none; ">
                         </div>
-                        <div class="col-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique animi dolore, nemo impedit et incidunt est ullam. Iure, recusandae molestiae!</div>
+                    </div>
+                    <div class="col-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique animi dolore, nemo impedit et incidunt est ullam. Iure, recusandae molestiae!</div>
                     </div>
                 </div>
                 <div class="aboutCol col">
                     <div class="row">
-                        <div class="col-1">
-                            <div class="aboutIcon">
-                                <img src="../design/images/arrowup.png" class="p-1" alt="..." style="height: 50px; width:50px;background:none; ">
-                            </div>
+                    <div class="col-1">
+                        <div class="aboutIcon">
+                            <img src="design/photos/arrowup.png" class="p-1" alt="..." style="height: 50px; width:50px;background:none; ">
                         </div>
-                        <div class="col-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique animi dolore, nemo impedit et incidunt est ullam. Iure, recusandae molestiae!</div>
+                    </div>
+                    <div class="col-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique animi dolore, nemo impedit et incidunt est ullam. Iure, recusandae molestiae!</div>
                     </div>
                 </div>
-
+            
             </div>
             <div class="aboutRow_sub row">
-
+            
                 <div class="aboutCol col">
                     <div class="row">
-                        <div class="col-1">
-                            <div class="aboutIcon">
-                                <img src="../design/images/arrowup.png" class="p-1" alt="..." style="height: 50px; width:50px;background:none; ">
-                            </div>
+                    <div class="col-1">
+                        <div class="aboutIcon">
+                            <img src="design/photos/arrowup.png" class="p-1" alt="..." style="height: 50px; width:50px;background:none; ">
                         </div>
-                        <div class="col-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique animi dolore, nemo impedit et incidunt est ullam. Iure, recusandae molestiae!</div>
+                    </div>
+                    <div class="col-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique animi dolore, nemo impedit et incidunt est ullam. Iure, recusandae molestiae!</div>
                     </div>
                 </div>
                 <div class="aboutCol col">
                     <div class="row">
-                        <div class="col-1">
-                            <div class="aboutIcon">
-                                <img src="../design/images/arrowup.png" class="p-1" alt="..." style="height: 50px; width:50px;background:none; ">
-                            </div>
+                    <div class="col-1">
+                        <div class="aboutIcon">
+                            <img src="design/photos/arrowup.png" class="p-1" alt="..." style="height: 50px; width:50px;background:none; ">
                         </div>
-                        <div class="col-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique animi dolore, nemo impedit et incidunt est ullam. Iure, recusandae molestiae!</div>
+                    </div>
+                    <div class="col-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique animi dolore, nemo impedit et incidunt est ullam. Iure, recusandae molestiae!</div>
                     </div>
                 </div>
-
+            
             </div>
             <div class="aboutRow_sub row">
-
+            
                 <div class="aboutCol col">
                     <div class="row">
-                        <div class="col-1">
-                            <div class="aboutIcon">
-                                <img src="../design/images/arrowup.png" class="p-1" alt="..." style="height: 50px; width:50px;background:none; ">
-                            </div>
+                    <div class="col-1">
+                        <div class="aboutIcon">
+                            <img src="design/photos/arrowup.png" class="p-1" alt="..." style="height: 50px; width:50px;background:none; ">
                         </div>
-                        <div class="col-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique animi dolore, nemo impedit et incidunt est ullam. Iure, recusandae molestiae!</div>
+                    </div>
+                    <div class="col-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique animi dolore, nemo impedit et incidunt est ullam. Iure, recusandae molestiae!</div>
                     </div>
                 </div>
                 <div class="aboutCol col">
                     <div class="row">
-                        <div class="col-1">
-                            <div class="aboutIcon">
-                                <img src="../design/images/arrowup.png" class="p-1" alt="..." style="height: 50px; width:50px;background:none; ">
-                            </div>
+                    <div class="col-1">
+                        <div class="aboutIcon">
+                            <img src="design/photos/arrowup.png" class="p-1" alt="..." style="height: 50px; width:50px;background:none; ">
                         </div>
-                        <div class="col-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique animi dolore, nemo impedit et incidunt est ullam. Iure, recusandae molestiae!</div>
+                    </div>
+                    <div class="col-10">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique animi dolore, nemo impedit et incidunt est ullam. Iure, recusandae molestiae!</div>
                     </div>
                 </div>
-
+            
             </div>
-
-
-
+           
+                
+            
 
         </div>
-
-
-
-
+        
+           
+            
+        
     </div>
 </div>
 

2bb1ae75b787dbdfcdbe9ca5cd7bdea8efc82715
Jheru <faustinojheru@gmail.com>
2023-02-13 09:54:06 +0800

    Landing, Login, About

---
+++ includes/navbar.php
@@ -0,0 +1,28 @@
+
+<nav class="navbar border text-bg-light shadow-lg navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">COMPANY NAME</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+  
+     <div class="container d-flex justify-content-end">
+      <ul class="navbar-nav justify-content-between gap-5">
+        <li class="btn" >
+          <a class="loginBtn_under nav-link active " aria-current="page" href="landingpage.php">Home</a>
+        </li>
+        <li class="btn">
+          <a class="loginBtn_under nav-link active" aria-current="page" href="aboutus.php">About Us</a>
+        </li>
+        <li class="btn">
+          <a class="loginBtn_under nav-link active" aria-current="page" href="#">Contacts</a>
+        </li>
+        <li class="btn ">
+          <a class="loginBtn nav-link active" aria-current="page" href="loginpage.php">Login</a>
+        </li>
+      </ul>
+     </div>
+
+  </div>
+</nav>

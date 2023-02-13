c977a942b35fa93d833ed4466323b5590e9d5e4c
darwin2919 <124544012+darwin2919@users.noreply.github.com>
2023-02-13 13:34:28 +0800

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,27 +1,34 @@
-<nav class="navbar border text-bg-light shadow-lg navbar-expand-lg bg-body-tertiary">
+
+<nav class="navbar border text-bg-light shadow-lg navbar-expand-lg bg-body-tertiary sticky-md-top">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">COMPANY NAME</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
-
-    <div class="container d-flex justify-content-end">
+  
+     <div class="container d-flex justify-content-end">
       <ul class="navbar-nav justify-content-between gap-5">
-        <li class="btn">
+
+        <li class="btn" >
           <a class="loginBtn_under nav-link active " aria-current="page" href="landingpage.php">Home</a>
         </li>
+
         <li class="btn">
           <a class="loginBtn_under nav-link active" aria-current="page" href="aboutus.php">About Us</a>
         </li>
+
         <li class="btn">
-          <a class="loginBtn_under nav-link active" aria-current="page" href="#">Contacts</a>
+          <a class="loginBtn_under nav-link active" aria-current="page" href="contacts.php">Contacts</a>
         </li>
+
+        
         <li class="btn ">
           <a class="loginBtn nav-link active" aria-current="page" href="loginpage.php">Login</a>
         </li>
+
       </ul>
-    </div>
+     </div>
 
   </div>
 </nav>

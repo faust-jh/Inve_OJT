06c30dc544e93cccd3d429e29f301f07e97f5d9f
Charlene-Doctor <charlenedoctor21@gmail.com>
2023-02-16 14:02:53 +0800

    inventory

--- inventory_dashboard/view3.php
+++ inventory_dashboard/view3.php
@@ -7,63 +7,58 @@ include '../includes/sidebardash.php';
   <div class="row col-md-12 m-auto">
     <h4>Dress</h4>
 
-<div class="text-end p-2">
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">+Add New Product</button>
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Add Product </h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <form>
-        <div class="mb-3 text-start">
-            <label for="recipient-name" class="col-form-label">Enter Product Name:</label>
-            <input type="text" class="form-control" id="recipient-name">
-          </div>
+    <div class="text-end p-2">
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">+Add New Product</button>
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Add Product </h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form class="px-3">
+                <div class="row my-2">
+                  <label for="" class="text-start"><b>Product Name</b></label>
+                  <input type="text" class="form-control" name="" placeholder="Enter Product Name">
+                </div>
+                <div class="row my-2">
+                  <label for="" class="text-start"><b>Select Color</b></label>
+                  <select class="form-select" aria-label="Default select example">
+                    <option value="1">Blue</option>
+                    <option value="2">Red</option>
+                    <option value="3">Green</option>
+                  </select>
+                </div>
+                <div class="row my-2">
+                  <label for="" class="text-start"><b>Select Size</b></label>
+                  <select class="form-select" aria-label="Default select example">
+                    <option value="1">Small</option>
+                    <option value="2">Medium</option>
+                    <option value="3">Large</option>
+                  </select>
+                </div>
+                <div class="row my-2">
+                  <label for="" class="text-start"><b>Select Brand</b></label>
+                  <select class="form-select" aria-label="Default select example">
+                    <option value="1">Blue Corner</option>
+                    <option value="2">Bench</option>
+                    <option value="3">Lacoste</option>
+                  </select>
+                </div>
+                <div class="row my-2">
+                  <label for="" class="text-start"><b>Select Photo</b></label>
+                  <input class="form-control" type="file" id="formFile">
+                  </select>
+                </div>
 
 
-         <div class="row text-start"> 
-            
-               <label for="recipient-name" >Select Color:</label>
-                 <select class="form-select" aria-label="Default select example">
-                       <option selected></option>
-                       <option value="1">Blue </option>
-                       <option value="2">Red</option>
-                       <option value="3">Green</option>
-                      <option value="4">Yellow</option>
-                    </select>
-              </div>
 
-<div class="row text-start">
-     <label for="recipient-name" >Select Size</label>
-<select class="form-select" aria-label="Default select example">
-  <option selected></option>
-  <option value="1">Small </option>
-  <option value="2">Medium</option>
-  <option value="3">Large</option>
-  <option value="4">XL</option>
-</select>
-
-</div>
-
-<div class="row text-start">
- <label for="recipient-name" >Select Brand:</label>
-<select class="form-select" aria-label="Default select example">
-  <option selected></option>
-  <option value="1">Bench</option>
-  <option value="2">Penshoppe</option>
-  <option value="3">Blue Corner</option>
-  <option value="4">Nike</option>
-</select>
-
-</div>
-        </form>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Save</button>
-        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
-      </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Save</button>
+                  <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
@@ -78,163 +73,118 @@ include '../includes/sidebardash.php';
   <thead>
     <tr>
       <th>Product ID</th>
-      <th>Product Name</th>
-      <th>Product Type</th>
-      <th>Product Size</th>
-      <th>Product Color</th>
-      <th>Item Quantity</th>
-      <th>Item Sold</th>
+      <th>Photo</th>
+      <th>Description</th>
+      <th>Quantity</th>
       <th>Manage</th>
     </tr>
   </thead>
 
-    <tbody>
-            <tr>
-                <td>D-0001</td>
-                <td>Dress-01</td>
-                <td>Cocktail</td>
-                <td> Large</td>
-                <td>Black</td>
-                <td>300</td>
-                <td>50</td>
-                <td> <button type="button" class="btn bg-primary text-white btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-pen-to-square"></i></button>&nbsp; &nbsp;<button class="btn bg-danger text-white btn-sm"><i class="fa-solid fa-trash"></i></button></td>   
-         
-            </tr>
+  <tbody>
+    <tr>
+      <td style="width: 100px;">D-0001</td>
+      <td style="width:85px"><img src="image/cocktail.jpeg" style="width: 85px;height:50px" alt="" srcset=""></td>
+      <td class="d-flex flex-column">
+        <div class="row">
+          <div class="col d-flex justify-content-end"><label><span class="text-muted">Name: </span>Dress</label></div>
+          <div class="col"><label><span class="text-muted">Size: </span>Medium</label></div>
+        </div>
+        <div class="row">
+          <div class="col d-flex justify-content-end"><label><span class="text-muted">Type: </span>Cocktail</label></div>
+          <div class="col"><label><span class="text-muted">Color: </span>Pink</label></div>
+        </div>
+      </td>
+      <td style="width:200px">
+        <label><span class="text-muted">Stocks: </span>500</label><br>
+        <label><span class="text-muted">Sold: </span>150</label>
+      </td>
+      <td style="width: 100px;"> <button type="button" class="btn bg-primary text-white btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-pen-to-square"></i></button>&nbsp; &nbsp;<button class="btn bg-danger text-white btn-sm"><i class="fa-solid fa-trash"></i></button></td>
 
-            <tr>
-                <td>D-0002</td>
-                <td>Dress-02</td>
-                <td>Cocktail</td>
-                <td>Medium</td>
-                <td>White</td>
-                <td>350</td>
-                <td>100</td>
-                <td> <button type="button" class="btn bg-primary text-white btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-pen-to-square"></i></button>&nbsp; &nbsp;<button class="btn bg-danger text-white btn-sm"><i class="fa-solid fa-trash"></i></button></td>   
-          
-            </tr>
+      <tr>
+      <td style="width: 100px;">D-0002</td>
+      <td style="width:85px"><img src="image/coocktail2.jpeg" style="width: 85px;height:50px" alt="" srcset=""></td>
+      <td class="d-flex flex-column">
+        <div class="row">
+          <div class="col d-flex justify-content-end"><label><span class="text-muted">Name: </span>Dress </label></div>
+          <div class="col"><label><span class="text-muted">Size: </span>Small</label></div>
+        </div>
+        <div class="row">
+          <div class="col d-flex justify-content-end"><label><span class="text-muted">Type: </span>Cocktail</label></div>
+          <div class="col"><label><span class="text-muted">Color: </span>Red</label></div>
+        </div>
+      </td>
+      <td style="width:200px">
+        <label><span class="text-muted">Stocks: </span>300</label><br>
+        <label><span class="text-muted">Sold: </span>200</label>
+      </td>
+      <td style="width: 100px;"> <button type="button" class="btn bg-primary text-white btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-pen-to-square"></i></button>&nbsp; &nbsp;<button class="btn bg-danger text-white btn-sm"><i class="fa-solid fa-trash"></i></button></td>
 
-            <tr>
-                <td>D-0003</td>
-                <td>Dress-03</td>
-                <td>Cocktail</td>
-                <td>XL</td>
-                <td>Blue</td>
-                <td>400</td>
-                <td>120</td>
-                <td> <button type="button" class="btn bg-primary text-white btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-pen-to-square"></i></button>&nbsp; &nbsp;<button class="btn bg-danger text-white btn-sm"><i class="fa-solid fa-trash"></i></button></td>   
-           
-            </tr>   
-           
-            <tr>
-                <td>D-0004</td>
-                <td>Dress-04</td>
-                <td>Cocktail</td>
-                <td>Large</td>
-                <td>Green</td>
-                <td>500</td>
-                <td>200</td>
-                <td> <button type="button" class="btn bg-primary text-white btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-pen-to-square"></i></button>&nbsp; &nbsp;<button class="btn bg-danger text-white btn-sm"><i class="fa-solid fa-trash"></i></button></td>   
-          
-            </tr>
+    </tr>
 
-            <tr>
-                <td>D-00005</td>
-                <td>Dress-05</td>
-                <td>Cocktail</td>
-                <td>Small</td>
-                <td>Yellow</td>
-                <td>600</td>
-                <td>560</td>
-                <td> <button type="button" class="btn bg-primary text-white btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-pen-to-square"></i></button>&nbsp; &nbsp;<button class="btn bg-danger text-white btn-sm"><i class="fa-solid fa-trash"></i></button></td>   
-            
-            </tr>
 
-            <tr>
-                <td>D-0006</td>
-                <td>Dress-06</td>
-                <td>Cocktail</td>
-                <td>Medium</td>
-                <td>Maroon</td>
-                <td>300</td>
-                <td>50</td>
-                <td> <button type="button" class="btn bg-primary text-white btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-pen-to-square"></i></button>&nbsp; &nbsp;<button class="btn bg-danger text-white btn-sm"><i class="fa-solid fa-trash"></i></button></td>   
-            
-            </tr>
 
-            <tr>
-                <td>D-0007</td>
-                <td>Dress-07</td>
-                <td>Formal</td>
-                <td>Large</td>
-                <td>Purple</td>
-                <td>350</td>
-                <td>100</td>
-                <td> <button type="button" class="btn bg-primary text-white btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-pen-to-square"></i></button>&nbsp; &nbsp;<button class="btn bg-danger text-white btn-sm"><i class="fa-solid fa-trash"></i></button></td>   
-            
-            </tr>
 
-            <tr>
-                <td>D-0008</td>
-                <td>Dress-08</td>
-                <td>Formal</td>
-                <td>Small</td>
-                <td>Orange</td>
-                <td>400</td>
-                <td>120</td>
-                <td> <button type="button" class="btn bg-primary text-white btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-pen-to-square"></i></button>&nbsp; &nbsp;<button class="btn bg-danger text-white btn-sm"><i class="fa-solid fa-trash"></i></button></td>   
-           
-            </tr>
-             
-            <tr>
-                <td>D-0009</td>
-                <td>Dress-09</td>
-                <td>Formal</td>
-                <td>Medium</td>
-                <td>Brown</td>
-                <td>500</td>
-                <td>200</td>
-                <td> <button type="button" class="btn bg-primary text-white btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-pen-to-square"></i></button>&nbsp; &nbsp;<button class="btn bg-danger text-white btn-sm"><i class="fa-solid fa-trash"></i></button></td>   
-          
-            </tr>
+      <tr>
+      <td style="width: 100px;">D-0003</td>
+      <td style="width:85px"><img src="image/formal.jpeg" style="width: 85px;height:50px" alt="" srcset=""></td>
+      <td class="d-flex flex-column">
+        <div class="row">
+          <div class="col d-flex justify-content-end"><label><span class="text-muted">Name: </span>Dress</label></div>
+          <div class="col"><label><span class="text-muted">Size: </span>medium</label></div>
+        </div>
+        <div class="row">
+          <div class="col d-flex justify-content-end"><label><span class="text-muted">Type: </span>Formal</label></div>
+          <div class="col"><label><span class="text-muted">Color: </span>Navy Blue</label></div>
+        </div>
+      </td>
+      <td style="width:200px">
+        <label><span class="text-muted">Stocks: </span>650</label><br>
+        <label><span class="text-muted">Sold: </span>400</label>
+      </td>
+      <td style="width: 100px;"> <button type="button" class="btn bg-primary text-white btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-pen-to-square"></i></button>&nbsp; &nbsp;<button class="btn bg-danger text-white btn-sm"><i class="fa-solid fa-trash"></i></button></td>
 
-            <tr>
-                <td>D-0010</td>
-                <td>Dress-010</td>
-                <td>Formal</td>
-                <td>Medium</td>
-                <td>Gray</td>
-                <td>600</td>
-                <td>560</td>
-                <td> <button type="button" class="btn bg-primary text-white btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-pen-to-square"></i></button>&nbsp; &nbsp;<button class="btn bg-danger text-white btn-sm"><i class="fa-solid fa-trash"></i></button></td>   
-          
-            </tr> 
+    </tr>
 
-            <tr>
-                <td>D-0011</td>
-                <td>Dress-011</td>
-                <td>Formal</td>
-                <td>Large</td>
-                <td>Pink</td>
-                <td>300</td>
-                <td>50</td>
-                <td> <button type="button" class="btn bg-primary text-white btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-pen-to-square"></i></button>&nbsp; &nbsp;<button class="btn bg-danger text-white btn-sm"><i class="fa-solid fa-trash"></i></button></td>   
-            </tr>
-          
 
-            <tr>
-                <td>D-0012</td>
-                <td>Dress-012</td>
-                <td>Formal</td>
-                <td>XL</td>
-                <td>Navy Blue</td>
-                <td>350</td>
-                <td>100</td>
-                <td> <button type="button" class="btn bg-primary text-white btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-pen-to-square"></i></button>&nbsp; &nbsp;<button class="btn bg-danger text-white btn-sm"><i class="fa-solid fa-trash"></i></button></td>   
-         
-            </tr>  
-    </tbody>
 
- 
+
+
+    <tr>
+      <td style="width: 100px;">D-0004</td>
+      <td style="width:85px"><img src="image/formal2.jpg" style="width: 85px;height:50px" alt="" srcset=""></td>
+      <td class="d-flex flex-column">
+        <div class="row">
+          <div class="col d-flex justify-content-end"><label><span class="text-muted">Name: </span>Dress</label></div>
+          <div class="col"><label><span class="text-muted">Size: </span>Small</label></div>
+        </div>
+        <div class="row">
+          <div class="col d-flex justify-content-end"><label><span class="text-muted">Type: </span>Formal</label></div>
+          <div class="col"><label><span class="text-muted">Color: </span>Yellow</label></div>
+        </div>
+      </td>
+      <td style="width:200px">
+        <label><span class="text-muted">Stocks: </span> 400</label><br>
+        <label><span class="text-muted">Sold: </span>150</label>
+      </td>
+      <td style="width: 100px;"> <button type="button" class="btn bg-primary text-white btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-pen-to-square"></i></button>&nbsp; &nbsp;<button class="btn bg-danger text-white btn-sm"><i class="fa-solid fa-trash"></i></button></td>
+
+    </tr>
+
+
+    <!-- <tr>
+      <td>D-0012</td>
+      <td>Dress-012</td>
+      <td>Formal</td>
+      <td>XL</td>
+      <td>Navy Blue</td>
+      <td>350</td>
+      <td>100</td>
+      <td> <button type="button" class="btn bg-primary text-white btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-pen-to-square"></i></button>&nbsp; &nbsp;<button class="btn bg-danger text-white btn-sm"><i class="fa-solid fa-trash"></i></button></td>
+
+    </tr> -->
+  </tbody>
+
+
   </tbody>
 
 </table>
